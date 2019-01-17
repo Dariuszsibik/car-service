ddbf25502186097670f450394b47434c1c86dd3d
Dariusz Sibik <sibikdariusz@gmail.com>
2019-01-17 23:59:29 +0100

    update angular @7.0.6

--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Car} from "./models/car";
-import {Observable} from "rxjs/Observable";
 import {Http, RequestOptions, Headers} from "@angular/http";
-import 'rxjs';
+import { Observable } from 'rxjs';
+//import 'rxjs/add/operator/map';
+import { map } from "rxjs/operators";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
 export class CarsService {
   private apiUrl = "http://localhost:3000/api/cars";
 
@@ -12,26 +16,26 @@ export class CarsService {
 
   getCars() : Observable<Car[]> {
     return this.http.get(this.apiUrl)
-      .map((res) => res.json())
+      .pipe(map((res) => res.json()))
   }
 
   getCar(id : number) : Observable<Car> {
     return this.http.get(this.apiUrl + `/${id}`)
-      .map((res) => res.json())
+    .pipe(map((res) => res.json()))
   }
 
   addCar(data) : Observable<Car> {
     return this.http.post(this.apiUrl, data)
-      .map((res) => res.json())
+    .pipe(map((res) => res.json()))
   }
 
   updateCar(id : number, data) : Observable<Car> {
     return this.http.put(this.apiUrl + `/${id}`, data)
-      .map((res) => res.json())
+    .pipe(map((res) => res.json()))
   }
 
   removeCar(id : number) : Observable<Car> {
     return this.http.delete(this.apiUrl + `/${id}`)
-      .map((res) => res.json())
+    .pipe(map((res) => res.json()))
   }
 }
