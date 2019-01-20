9e539124fff497ac8d528831b225862104d0076c
Dariusz Sibik <sibikdariusz@gmail.com>
2019-01-20 19:41:24 +0100

    add New Subject

--- src/app/cars/cost-shared.service.ts
+++ src/app/cars/cost-shared.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CostSharedService {
+  totalCostSource$ = new Subject<number>();
 
-  constructor() { }
+  shareCost(cost : number) {
+    this.totalCostSource$.next(cost);
+  }
 }
--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CostSharedService } from '../../cost-shared.service';
 
 @Component({
   selector: 'cs-income-tax',
@@ -9,9 +10,13 @@ export class IncomeTaxComponent implements OnInit {
   private incomeTax : number = 18;
   income : number;
 
-  constructor() { }
+  constructor(private costSharedService : CostSharedService,) { }
 
   ngOnInit() {
+    this.costSharedService.totalCostSource$.subscribe((cost) => {
+      this.income = cost * this.incomeTax / 100;
+      console.log(cost)
+    });
   }
 
 }
