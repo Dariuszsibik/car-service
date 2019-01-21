31583f78ca77086b821320e7ab270a56b6c08415
Dariusz Sibik <sibikdariusz@gmail.com>
2019-01-21 21:07:07 +0100

    add 3 fields to FormBuilder, array<object>

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -55,7 +55,12 @@ export class CarsListComponent implements OnInit {
       clientSurname: '',
       cost: '',
       isFullyDamaged: '',
-      year: ''
+      year: '',
+      parts: this.formBuilder.group({
+        name: '',
+        inStock: '',
+        price: ''
+      })
     });
   }
 
@@ -81,7 +86,10 @@ export class CarsListComponent implements OnInit {
   }
 
   addCar() {
-    this.carsService.addCar(this.carForm.value).subscribe(() => {
+    const carFormData = Object.assign({}, this.carForm.value);
+    carFormData.parts = [carFormData.parts];
+
+    this.carsService.addCar(carFormData).subscribe(() => {
       this.loadCars();
     });
   }
