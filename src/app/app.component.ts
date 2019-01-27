da43f45b2ac492b3bf66fa58917ef40644c0efc8
Dariusz Sibik <sibikdariusz@gmail.com>
2019-01-27 21:38:27 +0100

    add Spinner, RoutingEvent, Event i Route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from './shared-module/services/layout.service';
+import { Router, NavigationEnd, NavigationCancel, NavigationError, NavigationStart, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,28 @@ import { LayoutService } from './shared-module/services/layout.service';
 
 export class AppComponent implements OnInit{
   isSidebarVisible : boolean = false;
+  isLoading : boolean = false;
 
-constructor(private LayoutService : LayoutService) {}
+constructor(private LayoutService : LayoutService, private router : Router) {}
 
 ngOnInit() {
   this.LayoutService.sidebarSource$.subscribe((isVisible) => {
     this.isSidebarVisible = isVisible;
   })
+
+  this.router.events.subscribe((routerEvent : Event) => {
+    this.checkRouterEvent(routerEvent)
+  });
 }
 
+private checkRouterEvent(routerEvent: Event) {
+  if (routerEvent instanceof NavigationStart) {
+    this.isLoading = true;
+  } else if (routerEvent instanceof NavigationEnd ||
+    routerEvent instanceof NavigationCancel ||
+    routerEvent instanceof NavigationError) {
+    this.isLoading = false;
+  }
+}
 }
 
